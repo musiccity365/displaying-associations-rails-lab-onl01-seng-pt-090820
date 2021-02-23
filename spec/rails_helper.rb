5098d006c3e77b7365350a787fe09cc40b98d034
musiccity365 <m_sackstein@yahoo.com>
2021-02-22 19:32:49 -0500

    --- Auto-Git Commit ---

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,7 +42,7 @@ RSpec.configure do |config|
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
   #
-  # You can disable this behaviour by removing the line below, and instead
+  # You can disable this behavior by removing the line below, and instead
   # explicitly tag your specs with their type, e.g.:
   #
   #     RSpec.describe UsersController, :type => :controller do
